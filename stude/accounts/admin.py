b6a3ae56c373297d0aa62f46b596d020225a21a3
Keannu Bernasol <noehbernasol0@gmail.com>
2023-06-27 00:06:20 +0800

    Added relevant fields to Django admin panel

--- stude/accounts/admin.py
+++ stude/accounts/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('student_id_number',
+         'year_level', 'semester', 'avatar', 'is_banned')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
