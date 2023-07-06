07c57238a9bcd8d1ca005a107c833af3075dea89
Keannu Bernasol <noehbernasol0@gmail.com>
2023-07-07 01:25:15 +0800

    Fixed study group message serializer

--- stude/studygroup_messages/serializers.py
+++ stude/studygroup_messages/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Message
 from accounts.models import CustomUser
+from study_groups.models import StudyGroup
 
 
 class MessageSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
-        queryset=CustomUser.objects.all(), slug_field='full_name', required=False)
-    study_group = serializers.CharField(
-        source='subject.Subject', read_only=True, required=False)
+        queryset=CustomUser.objects.all(), slug_field='full_name', required=True)
+    study_group = serializers.SlugRelatedField(
+        queryset=StudyGroup.objects.all(), slug_field='name', required=True)
 
     class Meta:
         model = Message
