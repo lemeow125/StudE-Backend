2e4a52ededed1a88779c49d86057b12d1ff56d64
Keannu Bernasol <noehbernasol0@gmail.com>
2023-09-29 12:24:03 +0800

    Shorten student status url endpoints

--- stude/student_status/urls.py
+++ stude/student_status/urls.py
@@ -3,13 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .views import StudentStatusAPIView, ActiveStudentStatusListAPIView, StudentStatusListByStudentStatusLocation, StudentStatusListByCurrentLocation
 
 router = DefaultRouter()
-router.register(r'filter/near_current_location', StudentStatusListByCurrentLocation,
+router.register(r'near_current_location/', StudentStatusListByCurrentLocation,
                 basename='Student Status based on current location')
 
 urlpatterns = [
     path('self/', StudentStatusAPIView.as_view()),
     path('list/', ActiveStudentStatusListAPIView.as_view()),
-    path('filter/near_student_status',
+    path('near/',
          StudentStatusListByStudentStatusLocation.as_view()),
     path('', include(router.urls)),
 ]
